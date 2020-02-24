aa1d03b37ee980b58d95086fd718c360f5bf3675
susliko <1istoobig@gmail.com>
2020-02-24 16:25:31 +0300

    executor graceful termination

--- testing/src/main/scala/lotos/testing/Main.scala
+++ testing/src/main/scala/lotos/testing/Main.scala
@@ -6,8 +6,8 @@ import cats.effect.{ContextShift, ExitCode, IO, IOApp}
 import lotos.internal.model.{Gen, PrintLogs, Scenario}
 import lotos.internal.testing.{Invoke, TestRun, TestRunImpl}
 import lotos.testing.syntax.{method, spec}
-
 import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /*_*/
 object Main extends IOApp {
@@ -17,16 +17,18 @@ object Main extends IOApp {
       .withMethod(method("pop").throws[RuntimeException])
 
   val invoke: Invoke[IO] = LotosTest[IO].forSpec(stackSpec)
-  val scenario: Scenario = Scenario.gen(invoke.methods, 2, 5)
+  val scenario: Scenario = Scenario.gen(invoke.methods, 2, 50)
 
-  val ec                   = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2))
-  val cs: ContextShift[IO] = IO.contextShift(ec)
+  val executor = Executors.newFixedThreadPool(2)
+  val ec: ExecutionContext = ExecutionContext.fromExecutor(executor)
+  val cs: ContextShift[IO] = IO.contextShift(global)
   val testRun: TestRun[IO] = TestRunImpl[IO](invoke)(cs)
 
   override def run(args: List[String]): IO[ExitCode] =
     for {
       logs <- testRun.run(scenario)
       _    = println(PrintLogs.pretty(logs))
+      _ = executor.shutdown()
     } yield ExitCode.Success
 
 }
